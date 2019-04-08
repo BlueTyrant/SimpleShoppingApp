02fed9afb3b6a89fbd0c51a563961098de7e6c52
Blake <blakecarey07@gmail.com>
2019-04-08 16:06:09 +0100

    Initial Commit

---
+++ SimpleShoppingApp/Scripts/app.js
@@ -0,0 +1,39 @@
+﻿var currentList = {};
+
+function createShoppingList() {
+    currentList.name = $("#shoppingListName").val();
+    currentList.items = new Array();
+
+    showShoppingList();
+    //currentList.item = $();
+}
+
+function showShoppingList() {
+    $("#shoppingListTitle").html(currentList.name);
+    $("#shoppingListItems").empty();
+
+    $("#createListView").hide();
+    $("#shoppingListView").show();
+
+    $("#newItemName").focus();
+    $("#newItemName").keyup(function (event) {
+        if (event.keyCode == 13) {
+            addItem();
+        }
+    });
+
+}
+
+function addItem() {
+
+}
+
+$(document).ready(function () {
+    console.info("Ready!");
+    $("#shoppingListName").focus();
+    $("#shoppingListName").keyup(function (event) {
+        if (event.keyCode == 13) {
+            createShoppingList();
+        }
+    });
+})
